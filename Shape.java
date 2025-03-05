031ef12083c70209aa3fd1add4d27278f8f6c84c
aarushiimathur <148770634+aarushiimathur@users.noreply.github.com>
2025-03-05 11:38:54 +0530

    shape class
    created an abstract class for shape

--- Shape.java
+++ Shape.java
@@ -3,3 +3,14 @@ import java.util.*;
 interface Volume {
     double calculateVolume();
 }
+abstract class Shape{
+	protected double dim_one;
+	protected double dim_two;
+	protected double dim_three;
+	protected int num_sides;
+	
+	// zero parameter constructor
+	public Shape(){
+		dim_one = dim_two = dim_three = 0;
+		num_sides = 0;
+	}
