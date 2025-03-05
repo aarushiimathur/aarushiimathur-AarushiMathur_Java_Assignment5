edaea72903d5c55df3f5ab2eb6d56a3b7f5140b9
aarushiimathur <148770634+aarushiimathur@users.noreply.github.com>
2025-03-05 10:45:27 +0530

    calculateArea()
    Method for calculating area

--- Pyramid.java
+++ Pyramid.java
@@ -2,3 +2,9 @@ class Pyramid extends Shape implements Volume{
 	public Pyramid(double length, double width,double height){
 		super(length,width,height,5);
 	}
+	public double calculateArea(){
+		double baseArea = dim_one * dim_two; 
+        double slantHeight = Math.sqrt((dim_one / 2) * (dim_one / 2) + dim_three * dim_three);
+        double lateralArea = 2 * (dim_one * slantHeight / 2) + 2 * (dim_two * slantHeight / 2);
+        return baseArea + lateralArea;
+	}
