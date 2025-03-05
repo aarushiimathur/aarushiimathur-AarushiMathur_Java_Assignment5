d865b719f28964f6a503d43403225ee1fef9aaa9
aarushiimathur <148770634+aarushiimathur@users.noreply.github.com>
2025-03-05 10:43:55 +0530

    calculate volume and perimeter
    Method for calculating perimeter and volume of cylinder

--- Cylinder.java
+++ Cylinder.java
@@ -5,4 +5,11 @@ class Cylinder extends Shape implements Volume {
     public double calculateArea(){ 
 		return 2 * 3.14 * dim_one * (dim_one + dim_two); 
     }
+    public double calculatePerimeter(){ 
+		return 2 * 3.14 * dim_one; 
+	}
+    public double calculateVolume(){
+		return 3.14 * dim_one * dim_one * dim_two; 
+	}
+
 
