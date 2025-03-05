eafe1393865c3612ae8aedf1260467eef036e40c
aarushiimathur <148770634+aarushiimathur@users.noreply.github.com>
2025-03-05 10:42:37 +0530

    calculateArea()
    Method for calculating area

--- Cylinder.java
+++ Cylinder.java
@@ -1,4 +1,8 @@
 class Cylinder extends Shape implements Volume {
     public Cylinder(double radius, double height){ 
 		super(radius, height, 0, 0); 
-	}
+    }
+    public double calculateArea(){ 
+		return 2 * 3.14 * dim_one * (dim_one + dim_two); 
+    }
+
